0f224ebf6be02abab06aa9b8b18ba3c7606b3fd1
Nicolas Bisson <niclas035@hotmail.com>
2016-12-14 21:32:22 -0500

    Fix champ inscription + bouton retour inscription + fix build nicbiss + fix api pour test

--- app/src/main/java/com/example/etienne/scoreboard/InscriptionActivity.java
+++ app/src/main/java/com/example/etienne/scoreboard/InscriptionActivity.java
@@ -2,6 +2,8 @@ package com.example.etienne.scoreboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Nicolas on 2016-11-29.
@@ -12,5 +14,13 @@ public class InscriptionActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inscription);
+        Button boutonRetour = (Button) findViewById(R.id.boutonRetour);
+
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
