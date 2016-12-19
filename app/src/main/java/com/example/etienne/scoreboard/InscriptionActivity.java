33ce5c602caa46a5b39ccbb0611aebff8096490f
Marc <marc-plante1@hotmail.com>
2016-12-18 20:14:41 -0500

    j'ai creer les spinners biss

--- app/src/main/java/com/example/etienne/scoreboard/InscriptionActivity.java
+++ app/src/main/java/com/example/etienne/scoreboard/InscriptionActivity.java
@@ -3,7 +3,11 @@ package com.example.etienne.scoreboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Classe pour gérer le menu d'inscription.
@@ -34,5 +38,49 @@ public class InscriptionActivity extends Activity {
                 finish();
             }
         });
+
+        final ArrayList<String> numero = new ArrayList<String>() ;
+        for(int i = 1; i < 100; i++) {
+            String numeroStr = Integer.toString(i);
+            numero.add(numeroStr);
+        }
+
+        // Spinner des numeros et positions
+
+        final ArrayList<String> position = new ArrayList<String>() ;
+        position.add("AG");
+        position.add("C");
+        position.add("AD");
+        position.add("G");
+
+        Spinner numeroLoc = (Spinner)findViewById(R.id.NumeroJoueurLoc);
+        Spinner numeroVis = (Spinner)findViewById(R.id.NumeroJoueurVis);
+        Spinner positionLoc = (Spinner)findViewById(R.id.PositionJoueurLoc);
+        Spinner positionVis = (Spinner)findViewById(R.id.PositionJoueurVis);
+
+        ArrayAdapter<String> adapternumeroLoc = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, numero);
+        adapternumeroLoc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numeroLoc.setAdapter(adapternumeroLoc);
+
+        ArrayAdapter<String> adapternumeroVis = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, numero);
+        adapternumeroVis.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numeroVis.setAdapter(adapternumeroVis);
+
+        ArrayAdapter<String> adapterpositionLoc = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, position);
+        adapterpositionLoc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        positionLoc.setAdapter(adapterpositionLoc);
+
+        ArrayAdapter<String> adapterpositionVis = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, position);
+        adapterpositionVis.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        positionVis.setAdapter(adapterpositionVis);
+
+
+
+
+
+
+
+
+
     }
 }
